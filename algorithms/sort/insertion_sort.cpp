5bc6ce7104dc5d4231aa3f00f5302b24acc0d7f6
vishwashs4ngh <vikalp1077@gmail.com>
2024-09-01 13:16:59 +0530

    Modified  Sort

--- algorithms/sort/insertion_sort.cpp
+++ algorithms/sort/insertion_sort.cpp
@@ -1,7 +1,44 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std;\
+
+
+int insertionsort(int *A,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int temp=A[i];
+        int j=i-1;
+        while(j>=0 && A[j]>temp)
+        {
+            A[j+1]=A[j];
+            j--;
+        }
+        A[j+1]=temp;
+    }
+    return *A;
+}
+
+void display(int *A,int n)
+{
+   for(int i=0;i<n;i++)
+   {
+    printf("%d  ",A[i]);
+   }
+   printf("\n");
+}
 
 int main()
 {
-    
+      int A[]={5,4,10,1,6,2};
+      int n=6;
+
+      printf("Array before swapping:\n");
+      display(A,n);
+      insertionsort(A,n);
+      printf("Array after swapping:\n");
+      display(A,n);
+
+      return 0;
+
+
 }
